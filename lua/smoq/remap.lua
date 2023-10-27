038f430d4a81e02f7861bb0d408716a7b6013914
SmoQx <126472019+SmoQx@users.noreply.github.com>
2023-10-27 08:44:04 +0200

    Update remap.lua

--- lua/smoq/remap.lua
+++ lua/smoq/remap.lua
@@ -1,3 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>h", vim.cmd.nohls)
+vim.keymap.set('x', '<leader>y', '"+ygv<Esc><cmd>let @"=@0<CR>')
+vim.keymap.set('x', '<leader>Y', '"+Y<cmd>let @"=@0<CR>')
+vim.keymap.set({'n', 'x'}, '<leader>p', '"+p')
+vim.keymap.set({'n', 'x'}, '<leader>P', '"+P')
