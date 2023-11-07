c60cea3ef2ea3b9fc1f7ee9b132bd7fb2f7e0175
smoq <smoq@SmoQ>
2023-11-07 19:08:13 +0100

    added new remaps

--- lua/smoq/remap.lua
+++ lua/smoq/remap.lua
@@ -1,2 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>h", vim.cmd.nohls)
+vim.keymap.set('x', '<leader>y', '"+ygv<Esc><cmd>let @"=@0<CR>')
+vim.keymap.set('x', '<leader>Y', '"+Y<cmd>let @"=@0<CR>')
+vim.keymap.set({'n', 'x'}, '<leader>p', '"+p')
+vim.keymap.set({'n', 'x'}, '<leader>P', '"+P')
+vim.keymap.set({'n', 'x'}, '<Tab>' , '>gv')
+vim.keymap.set({'n', 'x'}, '<S-Tab>' , '<gv')
