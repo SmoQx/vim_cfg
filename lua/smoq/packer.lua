68872325a4bdc6a29063447c67b51814c7a14993
smoq <smoq@localhost.localdomain>
2024-01-03 07:34:43 +0100

    added ripgrep to pacer.lua added win to linux text conversion and linux to win text conversion using dos2unix

--- lua/smoq/packer.lua
+++ lua/smoq/packer.lua
@@ -21,6 +21,10 @@ return require('packer').startup(function(use)
         -- or                            , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     })
+    
+    use {
+        'BurntSushi/ripgrep'
+    }
 
          
     use {
