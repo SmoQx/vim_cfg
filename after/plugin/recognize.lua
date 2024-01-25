3183ecc51a3454a1dc3df759703d7a75b4dfaba9
SmoQx <smoqxpl@gmail.com>
2024-01-25 14:10:58 +0100

    test

---
+++ after/plugin/recognize.lua
@@ -0,0 +1,16 @@
+-- Function to setup Neovim based on file type
+local function setupNeovimForFileType()
+    local filetype = vim.fn.expand('%:e') -- Get file extension
+
+    if filetype == "python" then
+        print(filetype)
+    end
+end
+
+-- Setup autocmd to detect file type on BufRead and BufNewFile events
+vim.api.nvim_exec([[
+    augroup FileTypeDetection
+        autocmd!
+        autocmd BufRead,BufNewFile * call v:lua.setupNeovimForFileType()
+    augroup END
+]], false)
