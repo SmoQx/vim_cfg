1d3993b78a19efba37431cdc62832f096e45a82d
SmoQx <smoqxpl@gmail.com>
2024-01-29 08:54:36 +0100

    added recognicion script which adds a remap for formating and comments

--- after/plugin/recognize.lua
+++ after/plugin/recognize.lua
@@ -1,10 +1,21 @@
 -- Function to setup Neovim based on file type
-local function setupNeovimForFileType()
+function setupNeovimForFileType()
     local filetype = vim.fn.expand('%:e') -- Get file extension
-
-    if filetype == ".py" then
-        print(filetype)
+    local comment_symbol = '#'
+    local formatter = 'echo %'
+    if filetype == "py" then
+        comment_symbol = '#'
+        formatter = 'autopep8 --in-place --aggressive --aggressive %'
+    elseif filetype == "lua" then
+        comment_symbol = '--'
+    elseif filetype == "cs" then
+        comment_symbol = '//'
+        formatter = 'dotnet csharpier .'
+    elseif filetype == 'c' then
+        comment_symbol = '//'
     end
+    vim.keymap.set({'n', 'x', 'i'}, '<C-f><C-s>' , '<C-c>:!'..formatter..' <CR>')
+    vim.keymap.set('x', '<leader>c', ':s/^/'..comment_symbol..'<CR>:noh<CR>')
 end
 
 -- Setup autocmd to detect file type on BufRead and BufNewFile events
