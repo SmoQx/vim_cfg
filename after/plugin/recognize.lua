0d27585d1b968d6cf52349fbbcaefbcae7b881d0
SmoQx <smoqxpl@gmail.com>
2024-02-05 13:26:10 +0100

    added comment and format settings

--- after/plugin/recognize.lua
+++ after/plugin/recognize.lua
@@ -1,1 +1,4 @@
 local commnet = require("comment_and_format")
+commnet.setup({{ filetype = "lua", comment_symbol = "--", formatter = nil},
+              { filetype = "python", comment_symbol = "#", formatter = "autopep8 -i %"},
+              { filetype = "cs", comment_symbol = "\\/\\/", formatter = "dotnet csharpier %"}})
