2acddc21511ee3da05e884361da01516ede3d6a8
SmoQ <smoqxpl@gmail.com>
2024-02-06 20:42:04 +0100

    added reamap to recognize.lua

--- after/plugin/recognize.lua
+++ after/plugin/recognize.lua
@@ -1,4 +1,7 @@
 local commnet = require("comment_and_format")
-commnet.setup({{ filetype = "lua", comment_symbol = "--", formatter = nil},
+commnet.setup({{ filetype = "lua", comment_symbol = "--", formatter = ""},
               { filetype = "python", comment_symbol = "#", formatter = "autopep8 -i %"},
               { filetype = "cs", comment_symbol = "\\/\\/", formatter = "dotnet csharpier %"}})
+
+vim.keymap.set('x', '<leader>c', ':s/^/'..commnet.setupNeovimForFileType().comment_symbol..'<CR>:noh<CR>')
+vim.keymap.set({'n', 'x', 'i'}, '<C-f><C-s>' , '<C-c>:!'..commnet.setupNeovimForFileType().formatter..'<CR><CR>')
