7386ab7d8de0f2082ad221bd345e45d30a412b03
SmoQ <smoqxpl@gmail.com>
2024-02-10 20:18:18 +0100

    added c to recognize

--- after/plugin/recognize.lua
+++ after/plugin/recognize.lua
@@ -1,7 +1,8 @@
 local commnet = require("comment_and_format")
 commnet.setup({{ filetype = "lua", comment_symbol = "--", formatter = ""},
               { filetype = "python", comment_symbol = "#", formatter = "autopep8 -i %"},
-              { filetype = "cs", comment_symbol = "\\/\\/", formatter = "dotnet csharpier %"}})
+              { filetype = "cs", comment_symbol = "\\/\\/", formatter = "dotnet csharpier %"},
+              { filetype = "c", comment_symbol = "\\/\\/", formatter = ""}})
 
 vim.keymap.set('x', '<leader>c', ':s/^/'..commnet.setupNeovimForFileType().comment_symbol..'<CR>:noh<CR>')
 vim.keymap.set({'n', 'x', 'i'}, '<C-f><C-s>' , '<C-c>:!'..commnet.setupNeovimForFileType().formatter..'<CR><CR>')
