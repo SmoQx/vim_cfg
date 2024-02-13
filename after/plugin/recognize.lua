7a7dea07e68b47135b705fb89b240d6045d25a07
SmoQ <smoqxpl@gmail.com>
2024-02-13 19:46:01 +0100

    fixed error

--- after/plugin/recognize.lua
+++ after/plugin/recognize.lua
@@ -3,6 +3,15 @@ commnet.setup({{ filetype = "lua", comment_symbol = "--", formatter = ""},
               { filetype = "python", comment_symbol = "#", formatter = "autopep8 -i %"},
               { filetype = "cs", comment_symbol = "\\/\\/", formatter = "dotnet csharpier %"}})
 
-vim.keymap.set('x', '<leader>c', ':s/^/'..commnet.setupNeovimForFileType().comment_symbol..'<CR>:noh<CR>')
-vim.keymap.set({'n', 'x', 'i'}, '<C-f><C-s>' , '<C-c>:!'..commnet.setupNeovimForFileType().formatter..'<CR><CR>')
-vim.keymap.set('x', '<leader>uc', ':s/^'..commnet.setupNeovimForFileType().comment_symbol..'/<CR>:noh<CR>')
+function setKeyBind()
+    vim.keymap.set('x', '<leader>c', ':s/^/'..commnet.setupNeovimForFileType().comment_symbol..'<CR>:noh<CR>')
+    vim.keymap.set({'n', 'x', 'i'}, '<C-f><C-s>' , '<C-c>:!'..commnet.setupNeovimForFileType().formatter..'<CR><CR>')
+    vim.keymap.set('x', '<leader>uc', ':s/^'..commnet.setupNeovimForFileType().comment_symbol..'/<CR>:noh<CR>')
+end
+
+vim.api.nvim_exec([[
+    augroup FileTypeDetection
+        autocmd!
+        autocmd BufRead,BufNewFile,BufEnter * lua setKeyBind()
+    augroup END
+]], false)
